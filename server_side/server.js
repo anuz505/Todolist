39249cb2c9da3c275587921158b487e432785217
anuj bhandari <anujb364@gmail.com>
2024-10-10 13:16:17 +0545

    Refactor API endpoint origin to use production URL

--- server_side/server.js
+++ server_side/server.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "", // Replace with your frontend domain
+    origin: "https://todolist-delta-dusky.vercel.app", // Replace with your frontend domain
     credentials: true,
   })
 );

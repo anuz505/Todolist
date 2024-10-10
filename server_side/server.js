3bd877d7af6da20f05ca4359280104fe8afa0a29
anuj bhandari <anujb364@gmail.com>
2024-10-10 13:32:56 +0545

    Refactor API endpoint CORS configuration

--- server_side/server.js
+++ server_side/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://todolist-delta-dusky.vercel.app", // Replace with your frontend domain
+    methods: { POST, GET },
     credentials: true,
   })
 );
